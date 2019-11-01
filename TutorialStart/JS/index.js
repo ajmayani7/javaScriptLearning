89420f7bff2a6d2af16435420a6ec2aed5befada
My Name <shafiqanwarabbasi@gmail.com>
2019-11-01 17:41:14 +0500

    learning console.log

---
+++ TutorialStart/JS/index.js
@@ -0,0 +1,14 @@
+console.clear();
+console.time('your code Took');
+console.log('hello console');
+console.log(4+34);
+console.log(34);
+console.log(true);
+console.log([34,2,1,12]);
+console.table({Shafique : 'this', marks : 100});
+console.warn('this is a warnning');
+console.timeEnd('your code Took');
+// console.assert(566<189, 'Age > 189 is not possible');
+console.error('this is error');
+
+console.clear();
