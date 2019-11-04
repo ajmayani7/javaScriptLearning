f8dabfb7d9b726fd0c9d97368ea0f6853a4ff137
My Name <shafiqanwarabbasi@gmail.com>
2019-11-04 17:10:04 +0500

    tut2 page added

--- TutorialStart/JS/tut2.js
+++ TutorialStart/JS/tut2.js
@@ -1,1 +1,52 @@
-console.log('shafiq')
+console.log('tut3')
+
+//Variables in js
+//var, let, const
+
+var name = 'Abbasis';
+var channel;
+//channel = "code with Shafique";
+marks= 99;
+console.log(channel , name ,marks);
+
+//Rules for creating JavaScript Variables
+
+/* 
+1. Cannot start with numbers
+2. Can start with letters, numbers, _  or $
+3. Are case sensitive
+*/
+
+var city = 'karaci';
+console.log(city);
+
+const ownerName = 'Shafique';
+// ownerName = 'Abbasi'
+console.log(ownerName);
+
+const fruit = 'orange';
+console.log(fruit);
+
+{
+    let city = 'Islamabad';
+    city = 'Lahore';
+    console.log(city)
+}
+console.log(city)
+
+//Use array
+
+const arr1 = [12, 23, 12, 53, 3];
+arr1.push(45);
+console.log(arr1);
+
+/*
+Most common programming case types:
+
+1. camelCase
+2. kabab-case
+3. snake_case
+4. PascalCase
+
+
+*/
