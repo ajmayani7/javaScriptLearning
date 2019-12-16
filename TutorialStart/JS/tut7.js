b39eb678d3b3627f0f71807d368d49e9e2f7f528
Haseebbaig <haseebbaig12@hotmail.com>
2019-12-16 09:40:56 +0500

    data push

--- TutorialStart/JS/tut7.js
+++ TutorialStart/JS/tut7.js
@@ -1,1 +1,65 @@
 console.log("We are in tut7,js and lets discuss about arrays")
+
+const marks = [34, 23, 24, 93, 73, 25];
+const fruits = ['Orange', 'Apple', 'Pineapple'];
+const mixed = ['str', 89, [3, 5]];
+
+const arr = new Array(23, 123, 21, 'Orange');
+// console.log(arr);
+// console.log(mixed);
+// console.log(fruits[1]);
+// console.log(arr.length);
+// console.log(Array.isArray(arr));
+
+arr[0] = 'harry';
+arr[1] ='shafique';
+// console.log(arr)
+let arrelement = arr[0];
+// console.log('element :', arrelement);
+// let arrelement2 = arr[1];
+// console.log('element:', arrelement2);
+
+let value = marks.indexOf(93)
+// console.log(value)
+
+//  Mutating or Modifying arrays
+// marks.push(3567);
+// console.log(marks)
+
+// marks.unshift(2345);
+// console.log(marks);
+
+// marks.pop();
+// console.log(marks);
+
+// marks.shift()
+// console.log(marks);
+
+// marks.splice(1, 3);
+// console.log(marks);
+
+// marks.reverse()
+// console.log(marks);
+
+let score = [2, 4, 6, 8, 10];
+let score2 = [1, 3, 5, 7, 9];
+
+score = score.concat(score2)
+// console.log(score);
+
+// let myobj = {
+//     name : 'harry',
+//     channel: 'CodeWithHarry',
+//     isActive: true,
+//     marks: [1, 2, 3, 4, 5]
+// }
+// console.log(myobj.marks)
+
+let myobj = {
+    "first name" : 'harry',
+    channel: 'CodeWithHarry',
+    isActive: true,
+    marks: [1, 2, 3, 4, 5]
+}
+console.log(myobj['channel']);
+console.log(myobj.channel)
---
+++ TutorialStart/JS/tut8.js
@@ -0,0 +1,59 @@
+console.log('This is tut8');
+
+const age = 240;
+const doesDrive = true
+// const vari = 34;
+
+// if (age != 19) {
+//     console.log('age is 19')
+// }
+
+// else if(age == 24){
+//     console.log('age is 24')
+// }
+
+// else {
+//     console.log('age is not 24')
+// }
+
+
+// if ( typeof vari != 'undfined') {
+//     console.log('vari is defined');
+// } else {
+//     console.log('vari is not defined');
+// }
+
+// if (doesDrive && age >18) {
+//     console.log('you can drive');
+// } else {
+//     console.log('you can drive');
+// }
+
+// if (doesDrive || age > 2) {
+//     console.log('you can drive');
+// } else {
+//     console.log('you can not drive');
+// }
+
+// console.log(age == 18 ? 'you are great' : 'you are loser')
+
+
+// switch case
+
+// switch (age) {
+//     case 18:
+//             console.log('you are 18');
+//         break;
+
+//         case 24:
+//             console.log('you are 24');
+//         break;
+
+//         case 28:
+//             console.log('you are 28');
+//         break;
+
+//     default:
+//             console.log('you are unknown age');
+//         break;
+// }
